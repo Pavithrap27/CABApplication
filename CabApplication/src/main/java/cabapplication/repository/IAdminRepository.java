c06c25d851eb5c581d2a951c46c23512cc7e6996
Pavithra P <106738492+Pavithrap27@users.noreply.github.com>
2022-07-04 21:07:11 +0530

    push

--- CabApplication/src/main/java/cabapplication/repository/IAdminRepository.java
+++ CabApplication/src/main/java/cabapplication/repository/IAdminRepository.java
@@ -1,8 +1,10 @@
 package cabapplication.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import cabapplication.entity.Admin;
 import cabapplication.entity.Customer;
@@ -12,6 +14,8 @@ public interface IAdminRepository extends JpaRepository<Admin,Integer> {
 	public List<TripBooking> getTripsCabwise();
 	public List<TripBooking> getTripsCustomerwise();
 	public List<TripBooking> getTripsDatewise();
+	@Query("Select trips from TripBooking trips where customerId=? AND fromDate=? AND toDate=?")
+	public List<TripBooking> getAllTripsForDays(int customerId,LocalDateTime fromDate,LocalDateTime toDate);
 	
 
 }
