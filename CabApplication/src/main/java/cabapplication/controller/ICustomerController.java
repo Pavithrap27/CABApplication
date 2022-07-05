3205ceb40513dc2177230f869974f0f9cdaf11aa
v-yamini-saraswathi <vyaparla-yamini.saraswathi@capgemini.com>
2022-07-05 14:37:43 +0530

    pushing driver controller

--- CabApplication/src/main/java/cabapplication/controller/ICustomerController.java
+++ CabApplication/src/main/java/cabapplication/controller/ICustomerController.java
@@ -1,5 +1,28 @@
 package cabapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cabapplication.entity.Customer;
+import cabapplication.service.ICustomerService;
+
+@RestController
 public class ICustomerController {
 
+	@Autowired
+	ICustomerService customerservice;
+	
+	@PostMapping(path="/insertcustomer")
+	public Customer insertCustomer(Customer customer)
+	{
+		return customerservice.insertCustomer(customer);
+	}
+	@PutMapping(path="/updatecustomer")
+	public Customer updateCustomer(Customer customer)
+	{
+		return customerservice.updateCustomer(customer);
+	}
+	
 }
--- CabApplication/src/main/java/cabapplication/controller/IDriverController.java
+++ CabApplication/src/main/java/cabapplication/controller/IDriverController.java
@@ -1,10 +1,49 @@
 package cabapplication.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cabapplication.entity.Driver;
+import cabapplication.service.IDriverService;
+
 @RestController
+@RequestMapping("api")
 public class IDriverController {
 
+	@Autowired
+	IDriverService driverservice;
 	
-	
+	@PostMapping(path="/insertdriver")
+	public Driver insertDriver(Driver driver)
+	{
+		return driverservice.insertDriver(driver);
+	}
+	@PutMapping(path="/updatedriver")
+	public Driver updateDriver(Driver driver)
+	{
+		return driverservice.updateDriver(driver);
+	}
+	@DeleteMapping(path="/deletedriver{driverid}")
+	public String deleteDriver(@PathVariable int driverId)
+	{
+		return driverservice.deleteDriver(driverId);
+	}
+	@GetMapping(path="/viewbestdrivers")
+	public List<Driver> viewBestDrivers()
+	{
+		return driverservice.viewBestDrivers();
+	}
+	@GetMapping(path="/viewdriver{driverid}")
+	public Driver viewDriver(int driverid)
+	{
+		return driverservice.viewDriver(driverid);
+	}
 }
