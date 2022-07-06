3b94688e2fd78964e7f7942bfca5146d5bc91ed3
Siri-github <SHIREESHA.V@CAPGEMINI.COM>
2022-07-06 17:10:48 +0530

    commit

--- CabApplication/src/main/java/cabapplication/service/IAdminService.java
+++ CabApplication/src/main/java/cabapplication/service/IAdminService.java
@@ -3,8 +3,6 @@ package cabapplication.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import cabapplication.dto.AdminDTO;
 import cabapplication.dto.TripBookingDTO;
 import cabapplication.entity.Admin;
@@ -13,76 +11,28 @@ import cabapplication.exception.AdminNotFoundException;
 import cabapplication.exception.CabNotFoundException;
 import cabapplication.exception.CustomerNotFoundException;
 import cabapplication.exception.TripNotFoundException;
-import cabapplication.repository.IAdminRepository;
 
 public interface IAdminService {
 
-	@Autowired
-	IAdminRepository adminrepo;
-	ITripService tripservice;
-	
-	String message = "Admin not found";
 	public List<AdminDTO> getAll() throws AdminNotFoundException;
 
-	public List<Admin> getAdmin() throws AdminNotFoundException {
-		List<Admin> admin = adminrepo.findAll();
-		if (admin.isEmpty()) {
-			throw new AdminNotFoundException(message);
-		} else {
-			return admin;
-		}
-	}
 	public AdminDTO save(Admin admin) throws AdminNotFoundException;
 
-	public Admin insertAdmin(Admin admin) throws AdminNotFoundException {
-		if (admin == null) {
-			throw new AdminNotFoundException(message);
-		} else {
-			adminrepo.save(admin);
-			return admin;
-		}
+	
 	public AdminDTO update(Admin admin) throws AdminNotFoundException;
 
 	public String delete(Admin admin) throws AdminNotFoundException;
 
-	public Admin updateAdmin(Admin admin) throws AdminNotFoundException {
-		if (admin == null) {
-			throw new AdminNotFoundException(message);
-		} else {
-			int index = admin.getAdminId();
-			Admin adminupdated = adminrepo.findById(index).orElseThrow();
-			adminupdated.setEmail(admin.getEmail());
-			adminupdated.setMobileNumber(admin.getMobileNumber());
-			adminupdated.setPassword(admin.getPassword());
-			adminupdated.setUsername(admin.getUsername());
-			adminupdated.setAddress(admin.getAddress());
-			adminrepo.save(adminupdated);
-			return adminupdated;
-		}
-	}
+	
 	public AdminDTO getById(int adminId) throws AdminNotFoundException;
 
-	public String deleteAdmin(Admin admin) throws AdminNotFoundException {
-		if (admin == null) {
-			throw new AdminNotFoundException(message);
-		} else {
-			adminrepo.delete(admin);
-			return "Deleted";
-		}
+	
 	public List<TripBookingDTO> getByCustomerId(int customerId) throws CustomerNotFoundException;
 
 	public List<TripBookingDTO> getTripsCabwise() throws CabNotFoundException;
 
 	public List<TripBookingDTO> getTripsDatewise() throws TripNotFoundException;
 
-
-		Admin admin = adminrepo.findById(adminId).orElseThrow();
-		if (admin == null) {
-			throw new AdminNotFoundException(message);
-		} else {
-			return admin;
-		}
-	}
 	public List<TripBookingDTO> getTripsCustomerwise() throws CustomerNotFoundException;
 
 	public List<TripBooking> getAllTripsForDays() throws TripNotFoundException;
--- CabApplication/src/main/java/cabapplication/service/ICustomerService.java
+++ CabApplication/src/main/java/cabapplication/service/ICustomerService.java
@@ -2,97 +2,20 @@ package cabapplication.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import cabapplication.entity.Customer;
 
-@Service
-public class ICustomerService {
-	@Autowired
-	ICustomerRepository customerrepo;
-	
-	public List<Customer> getCustomer() throws CustomerNotFoundException
-	{
-		List<Customer> customer = customerrepo.findAll();
-		if(customer.isEmpty()) {
-			throw new CustomerNotFoundException("No Customer found");
-		} else {
-			return customer;
-		}
-	}
 public interface ICustomerService {
 
-	public Customer insertCustomer(Customer customer) throws CustomerNotFoundException
-	{
-		if (customer == null) {
-			throw new CustomerNotFoundException("No Customer found");
-		} else {
-			customerrepo.save(customer);
-			return customer;
-		}
-	}
 	public Customer insertCustomer(Customer customer);
-
-	public Customer updateCustomer(Customer customer) throws CustomerNotFoundException
-	{
-		if(customer == null) {
-			throw new CustomerNotFoundException("No Customer found");
-		}
-		else
-		{
-			int id = customer.getCustomerId();
-			Customer c = customerrepo.findById(id).orElseThrow();
-			c.setCustomerId(c.getCustomerId());
-			c.setEmail(customer.getEmail());
-			c.setAddress(customer.getAddress());
-			c.setMobileNumber(customer.getMobileNumber());
-			c.setPassword(customer.getPassword());
-			c.setUsername(customer.getUsername());
-			customerrepo.save(c);
-			return c;
-		}
-	}
+		
 	public Customer updateCustomer(Customer customer);
 
-	public String deleteCustomer(int customerId) throws CustomerNotFoundException
-	{
-		Customer customer = customerrepo.findById(customerId).orElseThrow();
-		if(customer == null)
-		{
-			throw new CustomerNotFoundException("No Customer found");
 	public String deleteCustomer(int customerId);
 
 	public List<Customer> viewCustomers();
 
-	public List<Customer> viewCustomers() throws CustomerNotFoundException {
-		List<Customer> customersList = customerrepo.findAll();
-		if(customersList.isEmpty())
-		{
-			throw new CustomerNotFoundException("No Customer found");
-		}
-		return customersList;
-	}
 	public Customer viewCustomer(int customerId);
 
-	public Customer viewCustomer(int customerId) throws CustomerNotFoundException 
-	{
-		Customer customer = customerrepo.findById(customerId).orElseThrow();
-		if(customer == null)
-		{
-			throw new CustomerNotFoundException("No Customer found");
-		}
-		return customer;
-	}
-
-	public Customer validateCustomer(String username, String password) throws CustomerNotFoundException
-	{
-		Customer customer = customerrepo.validateCustomer(username, password);
-		if(customer == null)
-		{
-			throw new CustomerNotFoundException("No Customer found");
-		}
-		return customer;
-	}
 	public Customer validateCustomer(String username, String password);
 }
-}
+
